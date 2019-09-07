30d642bb0c50590270c27df7bccbc4a113753041
windsmasher <paczka94@gmail.com>
2019-09-07 18:48:48 +0200

    configs not well but for interview

--- back/main.js
+++ back/main.js
@@ -2,16 +2,17 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3001;
+const config = require('./config/config');
+const PORT = process.env.PORT || config.port;
 const participantRoutes = require("./routes/participantRoutes");
 const bodyParser = require("body-parser")
 
 mongoose.connect("mongodb://gosc:123123q@ds233895.mlab.com:33895/events", { useNewUrlParser: true }, () => {
     console.log("Database connected")
 })
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/participant", participantRoutes)
-app.use(cors());
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
